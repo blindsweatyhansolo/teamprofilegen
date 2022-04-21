f181855ffe51ba3e31095885a8c77c6f4fefcab3
J Semidey <mtsemidey@gmail.com>
2022-04-21 16:06:46 -0400

    manager class getRole test fail-pass, overrides parent class getRole. Manager class tests completed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -16,3 +16,10 @@ test('get Manager office number', () => {
 
     expect(manager.getOfficeNum()).toBe(420);
 });
+
+// test if getRole() overrides from 'Employee' to 'Manager'
+test('get role of Manager, not Employee', () => {
+    const manager = new Manager("Jack", 1, "jack@gmail.com", 420);
+
+    expect(manager.getRole()).toEqual('Manager')
+});
