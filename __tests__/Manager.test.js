f1e42105d5a397e6c8b664338f04b3462d454a01
J Semidey <mtsemidey@gmail.com>
2022-04-21 15:57:21 -0400

    manager class getOfficeNum test fail-pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -9,3 +9,10 @@ test('inherits base values from Employee class', () => {
     expect(manager.id).toBe(manager.id);
     expect(manager.email).toBe(manager.email);
 });
+
+// test to get office number from Manager class using getOfficeNum()
+test('get Manager office number', () => {
+    const manager = new Manager("Jack", 1, "jack@gmail.com", 420);
+
+    expect(manager.getOfficeNum()).toBe(420);
+});
